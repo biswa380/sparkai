6e5f0beca21da05be8ba9cfa9f58a48e323e6351
Biswabhusan Pradhan <biswabhusanpradhan380@gmail.com>
2018-12-24 13:32:41 +0530

    project added

---
+++ src/main/java/org/sdrc/sparkai/sparkai/SparkaiApplication.java
@@ -0,0 +1,12 @@
+package org.sdrc.sparkai.sparkai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkaiApplication.class, args);
+	}
+}
